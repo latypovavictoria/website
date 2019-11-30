f42292df505c0efd582a6bee5ef046b0fdaf43b4
latypovavictoria <56290286+latypovavictoria@users.noreply.github.com>
2019-12-01 02:53:29 +0300

    Create site.php

---
+++ site.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="site.css">
+</head>
+<body>
+<h1>Хотите узнать больше о животных? Тогда Вам сюда!</h1>
+<?php 
+error_reporting(0);
+if ($_COOKIE['user']=='');
+?>
+<p><a href="reg.html">Зарегистрируйтесь, чтобы оставлять отзывы или комментарии</a></p><br>
+<p><a href="auth.html">Уже зарегистрированы? Войдите</a><p><br>
+<table style="table border:4px; height:900px; width:1200px;">
+<tr><td><h2>Разделы</h2></td>
+<td><h2>Виды</h2></td></tr>
+<tr>
+<td style="background-color:#FF8C00">Млекопитающие</td>
+<td style="background-color:#FF8C00"><a href="mammal.html">Перейти</a></td>
+</tr>
+<tr>
+<td style="background-color:#F0E68C">Птицы</td>
+<td style="background-color:#F0E68C"><a href="bird.php"></a>Перейти</td>
+</tr>
+<tr>
+<td style="background-color:#808000">Пресмыкающиеся</td>
+<td style="background-color:#808000"><a href="reptile.php"></a>Перейти</td>
+</tr>
+<tr>
+<td style="background-color:#006400">Земноводные</td>
+<td style="background-color:#006400"><a href="amfi.php"></a>Перейти</td>
+</tr>
+<tr>
+<td style="background-color:#20B2AA">Рыбы</td>
+<td style="background-color:#20B2AA"><a href="fish.php"></a>Перейти</td>
+</tr>
+<tr>
+<td style="background-color:#1E90FF">Моллюски</td>
+<td style="background-color:#1E90FF"><a href="moll.php"></a>Перейти</td>
+</tr>
+<tr>
+<td style="background-color:#8B4513">Насекомые</td>
+<td style="background-color:#8B4513"><a href="insect.php"></a>Перейти</td>
+</tr>
+<tr>
+<td style="background-color:#FFF0F5">Простейшие</td>
+<td style="background-color:#FFF0F5"><a href="simple.php"></a>Перейти</td>
+</tr>
+</table>
+<h3>Разработано ИУ4-11Б</h3>
+</body>
+</html>
