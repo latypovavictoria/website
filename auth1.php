177410900a129cac2fd6c4ab387e4a6844e6e36a
latypovavictoria <56290286+latypovavictoria@users.noreply.github.com>
2019-12-01 23:00:16 +0300

    Update and rename auth.html to auth1.php

--- auth1.php
+++ auth1.php
@@ -5,11 +5,16 @@
 <meta charset="utf-8">
 </head>
 <body>
+<?php 
+if($_COOKIE['user']==''): ?>
 <h1>Авторизация</h1>
 <form action="auth.php" method="post">
 <p>Ваш login: <input type="login" name="login"><br></p>
 <p>Ваш пароль: <input type="password" name="password"><br></p>
 <input type="submit" name="submit">
 </form>
+<?php else: ?>
+<p>Hello <?php=$_COOKIE['user']?>.Чтобы выйти нажмите <a href="/exit.php">здесь</a>.</p>
+<?php endif; ?>
 </body>
 </html>
