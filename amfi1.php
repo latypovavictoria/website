b8451d0bbfd468312a46d81c72cd9dcdd01182ad
latypovavictoria <56290286+latypovavictoria@users.noreply.github.com>
2019-12-08 21:28:49 +0300

    Update and rename amfi.html to amfi1.php

--- amfi1.php
+++ amfi1.php
@@ -21,6 +21,27 @@
 <li>Органы дыхания представлены лёгкими и кожей (у взрослых животных), у личинок сохраняется 
 жаберное дыхание (как у рыб).</li>
 </ol>
+  <?php
+$servername = "localhost"; 
+$username = "root";
+$password = "Vika_06012001";
+$dbname = "animal";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn) {
+   die("Ошибка подключения: " . mysqli_connect_error());
+}
+$sql = "SELECT text FROM animal3";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+   while($row = mysqli_fetch_assoc($result)) {
+       echo "" . $row["text"]."<br>";
+   }
+} else {
+   echo "0 результат";
+}
+mysqli_close($conn);
+?>
 <p1>Вот некоторые из земноводных</p1>
 <img src="frog.jpg" width="200"; height="200"><br>
 <img src="frog1.jpg" width="200"; height="200"><br>
