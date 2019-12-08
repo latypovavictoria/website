1eaed0771579629e57e9764c3ec219c9fc3f5869
latypovavictoria <56290286+latypovavictoria@users.noreply.github.com>
2019-12-08 21:38:16 +0300

    Create fish.php

---
+++ fish.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+* {background-color:#AFEEEE;
+}
+h1 {background-color:#00CED1; text-align:center;
+}
+.loi {text-align:center;
+}
+</style>
+<meta charset="utf-8">
+</head>
+<body>
+<h1>Дополнение статьи</h1>
+<div class="loi">
+<form action="" method="POST">
+<input type="text" name="text" placeholder="Новая информация" pattern="[A-Za-zА-Яа-я]{8,}"required>
+<input type="submit" value="Добавить">
+</form>
+</div>
+</body>
+</html>
+<?php
+$text=$_POST['text'];
+$host="localhost";
+$user="root";
+$password="Vika_06012001";
+$database="animal";
+$link=mysqli_connect($host, $user, $password, $database);
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+
+$sql="INSERT INTO `animal4` (`text`)
+VALUES('$text')";
+if (mysqli_query($link, $sql)) {
+   echo "Успешно создана новая запись";
+} else {
+  echo "Ошибка: " . $sql . "<br>" . mysqli_error($link);
+  echo "Ошибка: " . $sql . "<br>" . mysqli_error($link);
+  }
+  ?>
