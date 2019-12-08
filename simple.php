6f96e2c3cef44b1954f93fe980d9dff13023549e
latypovavictoria <56290286+latypovavictoria@users.noreply.github.com>
2019-12-08 21:43:02 +0300

    Create simple.php

---
+++ simple.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<style>
+* {background-color:#F0F8FF;
+}
+h1 {background-color:#FFC0CB; text-align:center;
+}
+.loi {text-align:center;
+}
+</style>
+<meta charset="utf-8">
+</head>
+<body>
+<h1>Дополнение статьи</h1>
+<div class="loi">
+<form action="" method="POST">
+<input type="text" name="text" placeholder="Новая информация" pattern="[A-Za-zА-Яа-я]{8,}"required>
+<input type="submit" value="Добавить">
+</form>
+</div>
+</body>
+</html>
+<?php
+$text=$_POST['text'];
+$host="localhost";
+$user="root";
+$password="Vika_06012001";
+$database="animal";
+$link=mysqli_connect($host, $user, $password, $database);
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+
+$sql="INSERT INTO `animal7` (`text`)
+VALUES('$text')";
+if (mysqli_query($link, $sql)) {
+   echo "Успешно создана новая запись";
+} else {
+  echo "Ошибка: " . $sql . "<br>" . mysqli_error($link);
+  echo "Ошибка: " . $sql . "<br>" . mysqli_error($link);
+  }
+  ?>
