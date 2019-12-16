53f6c222d3d59c1c2d3501fd22846e2d39b31bd2
latypovavictoria <56290286+latypovavictoria@users.noreply.github.com>
2019-12-16 21:00:49 +0300

    Update amfi.php

--- amfi.php
+++ amfi.php
@@ -1,44 +1,109 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8">
 <style>
-* {background-color:#98FB98;
+.mmm {border:2px solid #ADD8E6; height:250px; width:200px; display:block;
 }
-h1 {background-color:#8FBC8F; text-align:center;
+h1 {background-color:#32CD32; text-align:center;
 }
 .loi {text-align:center;
 }
+.poi {text-align:center;
+}
 </style>
-<meta charset="utf-8">
 </head>
 <body>
+<h1>Земноводные</h1>
+<h1 style="background: url(nice.jpg); height:200px; width:1500px; text-align:left; color:#228B22; padding:0px, 0px, 0px; margin:0px, 0px, 0px">Animals</h1>
+<p>Земноводные (они же амфибии) - первые появившиеся в процессе эволюции наземные позвоночные. 
+При этом они все еще сохраняют тесную связь с водной средой, обычно живя в ней на личиночной 
+стадии. Типичные представители земноводных - лягушки, жабы, тритоны, саламандры. Наиболее 
+разнообразны в тропических лесах, так как там тепло и сыро. Среди амфибий нет морских видов.</p>
+<li>скелет разделён на отчётливо выраженные отделы, появились шейный и крестцовый отделы 
+позвоночника.</li>
+<li>В связи с переходом к жизни на суше у них сформировались две пары пятипалых конечностей 
+рычажного типа.</li>
+<li>Головной мозг состоит из ствола и двух полушарий. Органы зрения, слуха и обоняния 
+приспособлены к наземному образу жизни (в дополнение к внутреннему появилось среднее ухо).</li>
+<li>У земноводных возник второй — лёгочный круг кровообращения и трёхкамерное сердце, 
+состоящее из двух предсердий и одного желудочка, в котором находится частично смешанная кровь.</li>
+<li>Органы дыхания представлены лёгкими и кожей (у взрослых животных), у личинок сохраняется 
+жаберное дыхание (как у рыб).</li>
+</ol>
+<?php
+$servername = "localhost"; 
+$username = "root";
+$password = "Vika_06012001";
+$dbname = "animal";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn) {
+   die("Ошибка подключения: " . mysqli_connect_error());
+}
+$sql = "SELECT text FROM animal1";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+   while($row = mysqli_fetch_assoc($result)) {
+       echo "" . $row["text"]."<br>";
+   }
+} else {
+   echo "0 результат";
+}
+mysqli_close($conn);
+?>
+<p style="color:#32CD32; text-align:center">Вот некоторые из земноводных</p>
+<table style="border: 3px solid #32CD32; margin:auto"><tr><td>
+<img src="frog.jpg" width="900"; height="500"></td></tr><tr><td>
+<img src="frog1.jpg" width="900"; height="500"></td></tr><tr><td>
+<img src="worm.jpg" width="900"; height="500"></td></tr>
+</table>
+<div class="mmm">
+<p style="color:#228B22; margin:10px; padding:10px">Животные</p>
+<ul>
+<li><a href="mammal1.php">Млекопитающие</a></li>
+<li><a href="bird1.php">Птицы</a></li>
+<li><a href="reptile1.php">Пресмыкающиеся</a></li>
+<li><a href="amfi1.php">Земноводные</a></li>
+<li><a href="fish1.php">Рыбы</a></li>
+<li><a href="moll1.php">Моллюски</a></li>
+<li><a href="insect1.php">Насекомые</a></li>
+<li><a href="simple1.php">Простейшие</a></li>
+<li><a style="color:#228B22" href="site.php">Веpнуться на главную</a></li>
+</ul>
+</div>
 <h1>Дополнение статьи</h1>
 <div class="loi">
-<form action="" method="POST">
-<input type="text" name="text" placeholder="Новая информация" pattern="[A-Za-zА-Яа-я]{8,}"required>
+<form action="amfid.php" method="POST">
+<input type="text" name="text" placeholder="Новая информация" pattern="{8,}"required>
 <input type="submit" value="Добавить">
 </form>
 </div>
-</body>
-</html>
+    <div class="poi">
+<h1 style>Удаление статьи</h1>
+<form action="" method="POST">
+<input type="text" name="id" placeholder="Введите id">
+<input type="submit" value="Удалить">
+</form>
 <?php
-$text=$_POST['text'];
+$id=$_POST['id'];
 $host="localhost";
 $user="root";
 $password="Vika_06012001";
 $database="animal";
-$link=mysqli_connect($host, $user, $password, $database);
+$link=new mysqli($host, $user, $password, $database);
 if (!$link) {
     die('Could not connect: ' . mysql_error());
 }
-echo 'Connected successfully';
-
-$sql="INSERT INTO `animal3` (`text`)
-VALUES('$text')";
+//echo 'Connected successfully';
+$sql=("DELETE FROM `animal3` WHERE `id`='$id'");
 if (mysqli_query($link, $sql)) {
-   echo "Успешно создана новая запись";
+   // echo "Запись успешно удалена";
 } else {
-  echo "Ошибка: " . $sql . "<br>" . mysqli_error($link);
-  echo "Ошибка: " . $sql . "<br>" . mysqli_error($link);
-  }
-  ?>
+   //echo "Ошибка удаления записи: " . mysqli_error($conn);
+}
+$link->close();
+?>
+<h3 style="background-color:#3CB371; text-align:center">Разработано ИУ4-11Б</h3>
+</body>
+</html>
